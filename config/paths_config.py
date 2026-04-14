be43fa5b6458af741b4ddb2dfa7dc5db94c68f5c
Raj_Utsav <utsavraj911@outlook.com>
2026-04-14 15:08:07 +0530

    improve the configs file path resolution

--- config/paths_config.py
+++ config/paths_config.py
@@ -1,18 +1,29 @@
-import os
+from pathlib import Path
 from dotenv import load_dotenv
+import os
+
 load_dotenv()
 
+# Base directory (project root safe)
+BASE_DIR = Path(__file__).resolve().parent
 
-RAW_DIR = "data/raw"
-PROCESSESED_DIR = "data/processed"
-RAW_FILE_PATH = os.path.join(RAW_DIR,"german_credit_data.csv")
-PROCESSED_FILE_PATH = os.path.join(PROCESSESED_DIR,"german_credit_data.csv")
+# Data directories
+RAW_DIR = BASE_DIR / "data" / "raw"
+PROCESSED_DIR = BASE_DIR / "data" / "processed"
 
-MODELS_DIR = "models"
-ENCODER_FILE_PATH = os.path.join(MODELS_DIR,"encoder.pkl")
-MODEL_FILE_PATH = os.path.join(MODELS_DIR,"model.pkl")
-SCALER_FILE_PATH = os.path.join(MODELS_DIR,"scaler.pkl")
+# File paths
+RAW_FILE_PATH = RAW_DIR / "german_credit_data.csv"
+PROCESSED_FILE_PATH = PROCESSED_DIR / "processed_data.csv"
 
+# Model paths
+MODELS_DIR = BASE_DIR / "models"
+ENCODER_FILE_PATH = MODELS_DIR / "encoder.pkl"
+MODEL_FILE_PATH = MODELS_DIR / "model.pkl"
+SCALER_FILE_PATH = MODELS_DIR / "scaler.pkl"
+
+# Environment variables
 API_KEY = os.getenv("API_KEY")
+
+# LLM config
 LLM_MODEL = "openai/gpt-oss-120b"
 BASE_URL = "https://api.groq.com/openai/v1"
